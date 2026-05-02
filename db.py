23ee7ecaa8963e821de7e7ad6eb41b0ee1d80701
sitanatarajan2006 <sitanatarajan3@gmail.com>
2026-05-02 14:27:29 +0100

    changed the UI style to match prior projects

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ def database():
     con = sqlite3.connect('northshore.db')
     cur = con.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS shipments (shipment_id INTEGER PRIMARY KEY, order_number TEXT, sender_details TEXT, receiver_details TEXT, item_description TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS deliveries (delivery_id INTEGER PRIMARY KEY, shipment_id INTEGER, delivery_date TEXT, assigned_driver TEXT, route_details TEXT, FOREIGN KEY (shipment_id) REFERENCES shipments(shipment_id))")
     con.commit()
     con.close()
 
@@ -11,6 +12,12 @@ def add_shipment(order_number, sender_details, receiver_details, item_descriptio
     con = sqlite3.connect('northshore.db')
     cur = con.cursor()
     cur.execute("INSERT INTO shipments (order_number, sender_details, receiver_details, item_description) VALUES (?, ?, ?, ?)", (order_number, sender_details, receiver_details, item_description))
+    con.commit()
+    con.close()
 
+def add_delivery(shipment_id, delivery_date, assigned_driver, route_details):
+    con = sqlite3.connect('northshore.db')
+    cur = con.cursor()
+    cur.execute("INSERT INTO deliveries (shipment_id, delivery_date, assigned_driver, route_details) VALUES (?, ?, ?, ?)", (shipment_id, delivery_date, assigned_driver, route_details))
     con.commit()
     con.close()
